52588807513ab952d0d4c88d6d33de577a00d454
Troels Thomsen <troels@thomsen.io>
2017-10-17 10:22:03 +0200

    Implement formatter middleware for scrubbing URLs

--- busltee/logger.go
+++ busltee/logger.go
@@ -1,10 +1,12 @@
 package busltee
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
+	"regexp"
 	"strings"
 
 	"github.com/sirupsen/logrus"
@@ -91,4 +93,23 @@ func logFields() *logrus.Entry {
 		return logrus.WithFields(logrus.Fields{})
 	}
 	return logrus.WithFields(l.defaultFields)
+}
+
+var urlRE = regexp.MustCompile("(?i)\\b((?:[a-z][\\w-]+:(?:/{1,3}|[a-z0-9%])|www\\d{0,3}[.]|[a-z0-9.\\-]+[.][a-z]{2,4}/)(?:[^\\s()<>]+|\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\))+(?:\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\)|[^\\s`!()\\[\\]{};:'\".,<>?«»“”‘’]))")
+
+type scrubber struct {
+	logrus.Formatter
+}
+
+func (s *scrubber) Format(entry *logrus.Entry) ([]byte, error) {
+	p, err := s.Formatter.Format(entry)
+	p = urlRE.ReplaceAllFunc(p, scrubURL)
+	return p, err
+}
+
+func scrubURL(url []byte) []byte {
+	if i := bytes.IndexRune(url, '?'); i >= 0 {
+		return append(url[:i], []byte("?...")...)
+	}
+	return url
 }
